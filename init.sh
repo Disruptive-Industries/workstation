bc26ceb00a38d0214e40a71500e53309616602fd
Antony Derham <antony.d@disruptive-industries.io>
2024-06-01 10:43:37 +0100

    feat: Create init script

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Homebrew and Ansible
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  source /etc/lsb-release
+  if [[ "$DISTRIB_ID" == "Ubuntu" ]]; then
+    sudo apt update
+    sudo apt install -y software-properties-common
+    sudo apt-add-repository --yes --update ppa:ansible/ansible
+    sudo apt install -y ansible
+  elif [[ "$DISTRIB_ID" == "Fedora" ]]; then
+    sudo dnf install -y ansible
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install ansible
+fi
+
+# Run Ansible playbook
+ansible-pull -U https://github.com/Disruptive-Industries/workstation
