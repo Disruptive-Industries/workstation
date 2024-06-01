416e927d6be95dee3b84b60cb18ad130ecc8c073
Antony Derham <antony.d@disruptive-industries.io>
2024-06-01 10:51:30 +0100

    fix: Use os-release to detect distro

--- init.sh
+++ init.sh
@@ -2,13 +2,13 @@
 
 # Install Homebrew and Ansible
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  source /etc/lsb-release
-  if [[ "$DISTRIB_ID" == "Ubuntu" ]]; then
+  source /etc/os-release
+  if [[ "$ID" == "ubuntu" ]]; then
     sudo apt update
     sudo apt install -y software-properties-common
     sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt install -y ansible
-  elif [[ "$DISTRIB_ID" == "Fedora" ]]; then
+  elif [[ "$ID" == "fedora" ]]; then
     sudo dnf install -y ansible
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
